cadb294883503e3cd957db0a8ea6e2e0a6fbb722
va164 <victor.ahluwalia@gmail.com>
2016-05-17 19:40:18 -0400

    added functionality for new HTML elements using JavaScript

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -1,7 +1,13 @@
 $(document).ready( function () {
 
 	console.log("ready!");
-	$(".test").prepend ('<p>new text!</p>');
-	console.log("changed html");
 
+	// creating variables 
+	var para = document.createElement("p");
+	var test = document.getElementById("test");
+
+	// adding new elements to html
+	para.innerHTML = "This is sample text";
+	test.appendChild(para);
+	console.log("added sample text to a new para element");
 });
