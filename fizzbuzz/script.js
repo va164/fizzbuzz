e411f49bf5d0bcf6c1b79f0ccb17cc0082097472
va164 <victor.ahluwalia@gmail.com>
2016-05-28 10:52:50 -0400

    completed fizzbuzz refactor

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -1,15 +1,41 @@
 $(document).ready( function () {
-
+	
 	// creating variables 
 	var newPara = document.createElement("p"); // New paragraph
 	var test = document.getElementById("test"); // Existing Section
 	var text = ""; // Variable for text
 	var i= 0; // Variable for number count
+	var uInput = ""; // Variable for storing user provided number
+	
+
+/*
+	// If type of input is not number, ask again
+	while (typeof uInput !== "number") {
+		uInput = prompt("Please provide an integer between 1 and 100 in NUMERICAL FORMAT");	//works with parseInt incorrectly, otherwise does not work at all
+		console.log('Re-asked for user input');
+	};
+
+	if (uInput % 1 != 0) {
+		uInput = +uInput;
+		console.log(uInput); //not working
+	};
+*/
 	
 	
+	Math.ceil(uInput);
+	do {
+		uInput = prompt("Please provide an integer between 1 and 100");
+		 var vInput = Math.ceil(uInput);
+		//add functionality to ask again
+		console.log("initial entry was not a number");
+	}
+	while ( vInput % 1 !== 0 );
 	
+
+
+
 	// loop to list 100 numbers
-	while (i<100) {
+	while (i< vInput) {
 		i++; // increment by 1
 	
 		var three = i % 3; // remainder when divided by 3
